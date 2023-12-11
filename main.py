371edf35440f9531ea6d98fbfb53c7662e01513a
djumanov <diyorbekjumanov1120@gmail.com>
2023-12-11 10:37:29 +0500

    echo

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
-from dotenv import load_dotenv
-import os
+from settings import TOKEN
+from telegram import Bot
+import time
 
-load_dotenv()
+bot = Bot(TOKEN)
 
-TOKEN = os.getenv('TOKEN')
-print(TOKEN)
+last_update_id = -1
+while True:
+    curr_update = bot.getUpdates()[-1]
+
+    if last_update_id != curr_update.update_id:
+
+        user    = curr_update.message.from_user
+        message = curr_update.message
+
+        bot.sendMessage(user.id, message.text)
+
+        last_update_id = curr_update.update_id
+    
+    time.sleep(0.5)
+    
